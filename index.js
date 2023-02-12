cd74addda147fe25e4472d45eac75636e881af54
Vinso <8vinso8@gmail.com>
2023-02-13 02:34:06 +1000

    Delete test

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const {Client, GatewayIntentBits, Events} = require('discord.js');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+  intents: [GatewayIntentBits.Guilds],
 });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("messageCreate", (message) => {
-  if (message.content === "ping") {
-    message.reply("Pong!");
-  }
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
